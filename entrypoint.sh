b71eecb09170fccfb4be75da666cfb31868b792b
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2025-10-07 19:14:02 +0300

    Добавил установку composer

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,13 @@ fi
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+
+# Устанавливаем Composer, если его нет
+if ! command -v composer >/dev/null 2>&1; then
+    echo "Composer не найден — устанавливаем..."
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+fi
+
 # -------------------
 # Установка зависимостей Composer
 # -------------------
