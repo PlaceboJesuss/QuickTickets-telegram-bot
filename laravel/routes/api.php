94b7b88dabcb2babfa4b1fd1043c088921c4de71
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2025-10-09 00:29:23 +0300

    test

--- laravel/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// routes/api.php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TelegramWebhookController;
-
-Route::post('/tg_webhook', [TelegramWebhookController::class, 'handle']);
