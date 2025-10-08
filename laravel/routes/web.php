041ef1d3f169b3b85310693ebf59055db53a417d
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2025-10-09 00:52:43 +0300

    моя попытка номер 5

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('/tg_webhook', [TelegramWebhookController::class, 'handle'])
-     ->withoutMiddleware([\Illuminate\Session\Middleware\StartSession::class]);
